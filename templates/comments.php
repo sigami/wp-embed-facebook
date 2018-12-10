efbcc30e37d807db1b2140582817280781c01059
miguel <miguel.sirvent@gmail.com>
2018-12-09 22:32:30 -0600

    force 100% length on comments template

--- templates/comments.php
+++ templates/comments.php
@@ -3,4 +3,5 @@ use SIGAMI\WP_Embed_FB\Social_Plugins;
 if ( post_password_required() ) {
 	return;
 }
+echo "<style>.fb_iframe_widget iframe{width: 100% !important;}</style>";
 echo Social_Plugins::get('comments',array('href'=>wp_get_shortlink(get_queried_object_id()),'width'=>'100%'));
