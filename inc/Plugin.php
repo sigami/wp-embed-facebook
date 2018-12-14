bea280161b0de945f37febb65299a39e6a53e8c2
miguel <miguel.sirvent@gmail.com>
2018-12-13 23:56:16 -0600

    Update v3.0.5

--- inc/Plugin.php
+++ inc/Plugin.php
@@ -19,7 +19,7 @@ if ( ! defined( 'WPINC' ) ) {
  */
 final class Plugin extends Framework {
 
-	const VER = '3.0.4';
+	const VER = '3.0.5';
 
 	static $option    = 'wpemfb_options';
 	static $menu_slug = 'embedfacebook';
